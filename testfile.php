eaa9030bc58d84bdc64e1a7643ec9a644a09876b
semiller <semillerjr@gmail.com>
2017-05-09 08:39:40 -0400

    added testfile.php

---
+++ testfile.php
@@ -0,0 +1,84 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="description" content="The Manage Log Service Toolkit is designed with the service manager in mind.  Let us assist with your daily tracking of all your daily activities and operations. ">
+<meta name="keywords" content="manage, log, toolkit, service, tracker, training, safety, projects, work orders, journal">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" > 
+<title>Manage Log | FAQ</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="js/jquery.js"></script>
+</head>
+<body>
+<div class="main">
+  
+  <?php include 'inc/header.inc.php'; ?>
+  <div class="clr"></div>
+  <?php include 'inc/menu.inc.php'; ?>
+  
+  <?php include 'inc/banner.inc.php' ?>
+  
+  
+   
+  <div class="clr"></div>
+  
+  <div class="body">
+    <div class="body_content">
+     
+      
+      <div class="left">
+        <h2>Frequently Asked Questions</h2>
+        <p>Listed below are a listed of our most frequently asked questions regarding our services and membership.</p>
+        
+        <h3>Registration / Payment</h3>
+        
+        <p><strong>How do I register for membership?</strong></p>
+        <p>Registration is completed by clicking <a href="toolkit/register.php" target="_blank">'Register New Account'</a> on the top right portion of the site.  After entering all required fields, you will be redirected to a payment page for processing.  Upon successful registration, we will email your login credentials and you will be able to successfully log in and begin using our service toolkit.</p>
+        
+        <p><strong>How do I pay for your service?</strong></p>
+        <p>Paypal is used for all transactions to gain access and membership to our service.  Payment to use our service is a one time payment (not a subscription service)</p>
+        
+        <h3>Membership</h3>
+        
+        <p><strong>What is included with my membership?</strong></p>
+        <p>As a member of the Manage Log Service Toolkit, you are given complete access to our toolkit to track everything from employees, training, projects, work orders, and safety tracking.</p>
+        
+        <p><strong>How about upgrades and enhancements?</strong></p>
+        <p>We are continuing to add improvements and enhancements over time.  As a member, you are given complete access to all upgrades free of charge.</p>
+        
+        <h3>Service Toolkit - Operation</h3>
+        
+        <p><strong>How do I get started?</strong></p>
+        <p>As the registered member/manager, you are the set admin for the account/facility name you create.  This gives you the ability to create an unlimited number of departments and managers for your location.  The first step in setting up your account is to create departments, followed by management staff for those departments.</p>
+        
+        <p><strong>How do I grant access to my management staff?</strong></p>
+        <p>Once you create new managers for your departments, they will be notified via email that you have created login credentials for them to start using our service.  The email will contain the username and password required for management login.  Once logged in, managers can view their profile page and change their password.</p>
+        
+        <p><strong>What are the benefits of the Admin account?</strong></p>
+        <p>As the Admin member, you have full access to view content from all of the departments and managers.  You have the access to create, edit, and delete content from all departments.  The Admin account can also create global categories that all departments will have the benefit of using.</p>
+        
+        <p><strong>Why am I getting a warning message?</strong></p>
+        <p>The warning messages are designed to inform you that categories must first be created before continuing with the adding of content.  Once categories are in place, the warning messages will no longer appear.</p>
+        
+        <p><strong>Can the management staff see each other manager's content?</strong></p>
+        <p>No.  Each manager only has the ability to view their own content.  If a manager creates assistant managers to run the operation with them, they will only be able to view content related to their respective departments.</p>
+        
+        <p><strong>Do you have additional questions that are not addressed?<br>  Feel free to <a href="contact.php">contact us</a> and we will gladly assist.</strong></p>
+       
+      <!-- end .left --></div>
+      
+      
+      <div class="right">
+      
+      <?php include 'inc/join_today.inc.php'; ?>
+      <?php include 'inc/tracking_features.inc.php'; ?>
+        
+      <!-- .right --></div>
+      <div class="clr"></div>
+    <!-- end .body_content --></div>
+  <!-- end .body --></div>
+<!-- end .main --></div>
+
+<?php include 'inc/footer.inc.php'; ?>
+</body>
+</html>
