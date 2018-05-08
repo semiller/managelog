766a27dc4f57bba7ae475b02928ad08ab7da2532
semiller <semillerjr@gmail.com>
2018-05-08 13:11:19 +0000

    Update connect.inc.php

--- inc/connect.inc.php
+++ inc/connect.inc.php
@@ -1,10 +1,10 @@
 <?php
 	// connect to DB
 $config = array(
-	'host'		=> '23.229.156.96',
-	'username'	=> 'smilljr',
-	'password'	=> 'optimus1',
-	'dbname' 	=> 'managelog'
+	'host'		=> '',
+	'username'	=> '',
+	'password'	=> '',
+	'dbname' 	=> ''
 );
 // set up mysqli connection
 $conn = new mysqli($config['host'],$config['username'],$config['password'],$config['dbname']);
